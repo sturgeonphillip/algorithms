d15e2edb291dd7d25f6c0855747354f06dd57a1f
Phillip Sturgeon <sturgeonphillip@protonmail.com>
2025-05-13 15:34:12 -0700

    9 billion names of God

--- fcc/rosetta/100Doors.js
+++ fcc/rosetta/100Doors.js
@@ -7,7 +7,7 @@ function getFinalOpenedDoors(numDoors) {
   while (p <= numDoors) {
     for (let i = 1; i <= numDoors; i++) {
       // count for 0 index
-      let status = doors[i -1]
+      let status = doors[i - 1]
 
       // change every p doors on pass
       if (i % p === 0) {
@@ -26,4 +26,12 @@ function getFinalOpenedDoors(numDoors) {
   }).filter((x) => x !== false);
 }
 
-console.log(getFinalOpenedDoors(100));
+
+console.log(getFinalOpenedDoors(100))
+/**
+ * 100 Doors
+ * 
+ * There are 100 doors in a row that are all initially closed. You make 100 passes by the doors. The first time through, visit every door and 'toggle' the door (if the door is closed, open it; if it is open, close it). The second time, only visit every 2nd door (i.e., door #2, #4, #6, ...) and toggle it. The third time, visit every 3rd door (i.e., door #3, #6, #9, ...), etc., until you only visit the 100th door.
+
+Implement a function to determine the state of the doors after the last pass. Return the final result in an array, with only the door number included in the array if it is open.
+ */
